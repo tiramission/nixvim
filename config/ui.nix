5667890a00574cb53f912419e059092113ed548b
tiramission <37465086+tiramission@users.noreply.github.com>
2026-01-06 16:57:26 +0800

    opt: config common

--- config/ui.nix
+++ config/ui.nix
@@ -1,3 +1,16 @@
 {
   colorschemes.catppuccin.enable = true;
+
+  opts = {
+    number = true;
+    clipboard = "unnamedplus";
+    tabstop = 2;
+    softtabstop = 2;
+    expandtab = true;
+    smartindent = true;
+    shiftwidth = 2;
+    breakindent = true;
+    mouse = "a";
+    swapfile = false;
+  };
 }
